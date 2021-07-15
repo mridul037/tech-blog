58d0a184f391f263090d8e573e56016ee3fb2458
mridul037 <mridulshukla037@gmail.com>
2021-07-15 17:26:49 +0530

    chnages

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ const IndexPage = () => {
               them
             </li>
             <li className={styles.item}>
-              Will Include some article about Coding-development,data analysis
+              Will Include some article about Coding,System Design,Vue,React,Python,Javascript,data analysis
               and other stuff.
             </li>
             {/* <li style={item}>
